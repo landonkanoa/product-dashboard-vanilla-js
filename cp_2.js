3de8780d76392c0732898f1f880cab9e5ba16cf2
Landon <landonlopez@usf.edu>
2025-10-18 19:53:09 -0400

    Changed it so dP(p) and hE(e) were outside the async function.

--- cp_2.js
+++ cp_2.js
@@ -15,21 +15,23 @@ function fetchProductsThen() {
 // Added async function and displayProduct to show in console log and handleError if there is a problem loading the API
 async function fetchProductsAsync() {
     try {
-        const res = await fetch("https://www.course-api.com/javascript-store-product")
+        const res = await fetch("https://www.course-api.com/javascript-store-products")
         if (!res.ok) throw new Error(`error code: ${res.status}`);
         const data = await res.json();
-        function displayProducts(products) {
-            products.forEach((product) => {
-                console.log(product);                
-            });
-        }
         displayProducts(data);      
     } catch (error) {
-        function handleError(error) {
-            console.error("Problem getting items", error.message);
-        }
         handleError(error);
     }
+};
+
+function displayProducts(products) {
+    products.forEach((product) => {
+        console.log(product);                
+    });
+};
+
+function handleError(error) {
+    console.error("Problem getting items", error.message);
 };
 
 
