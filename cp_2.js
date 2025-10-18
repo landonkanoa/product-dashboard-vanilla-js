bdf32ff0770bc018c8e429588f638fa25a2c797c
Landon <landonlopez@usf.edu>
2025-10-18 19:46:36 -0400

    Created async function that displays products and or error message.

--- cp_2.js
+++ cp_2.js
@@ -12,4 +12,24 @@ function fetchProductsThen() {
     })
 };
 
+// Added async function and displayProduct to show in console log and handleError if there is a problem loading the API
+async function fetchProductsAsync() {
+    try {
+        const res = await fetch("https://www.course-api.com/javascript-store-product")
+        if (!res.ok) throw new Error(`error code: ${res.status}`);
+        const data = await res.json();
+        function displayProducts(products) {
+            products.forEach((product) => {
+                console.log(product);                
+            });
+        }
+        displayProducts(data);      
+    } catch (error) {
+        function handleError(error) {
+            console.error("Problem getting items", error.message);
+        }
+        handleError(error);
+    }
+};
+
 
