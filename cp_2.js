8654ad49edf83401af46a3e659010f263b498f30
Landon <landonlopez@usf.edu>
2025-10-18 18:24:36 -0400

    Added some HTML and created fetch function for items

--- cp_2.js
+++ cp_2.js
@@ -1,3 +1,15 @@
+// Added function that fetches products from API and console logs all the items
 function fetchProductsThen() {
     fetch("https://www.course-api.com/javascript-store-products")
-}
+    .then((res) => res.json())
+    .then((items) => {
+        items.forEach((product) => {
+            console.log(product);           
+        })
+    })
+    .catch(() => {
+        console.log("Error loading data.");        
+    })
+};
+
+
